d682142413e51a932ac6b354705231b8576c825a
LoafOfCode <53025614+LoafOfCode@users.noreply.github.com>
2022-05-12 17:06:56 -0700

    finished player movement script

--- Scripts/Player/PlayerMovement.gd
+++ Scripts/Player/PlayerMovement.gd
@@ -6,15 +6,14 @@ var velocity = Vector2.ZERO
 func _physics_process(delta):
 	var movement = Vector2(
 			# Calculate X strength
-			Input.get_action_strength("right") - Input.get_action_strength("left"),
+			Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 			#Calculate Y strength
-			Input.get_action_strength("down") - Input.get_action_strength("up")
+			Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 		)
 	if(movement.length() > 1.0): 
 		movement = movement.normalized()
-	move_and_slide(movement)
+	move_and_slide(movement * speed)
 	pass 
-	
 
 func _ready():
 	pass
